5bc178cb39dc3f0ae05b48e2f033999f2bd13c1c
Howon <howonbyun@gmail.com>
2015-09-25 19:19:36 -0400

    package file uploaded

---
+++ scripts/routes.js
@@ -0,0 +1,14 @@
+var React = require('react/addons'),
+	HomePage = React.createFactory(require('./render/pages/js/HomePage')),
+
+
+module.exports = function (app){
+	app.get('/', function (req, res) {
+
+		var reactBody = React.renderToString(HomePage());
+		res.render('index', {
+			title     : 'Raymond',
+			body 	  : reactBody,
+		});
+	})
+}

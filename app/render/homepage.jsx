960d06463fdf02040d36f492fee7c3fb4ca9ea1c
Howon <howonbyun@gmail.com>
2015-10-23 18:22:59 -0400

    side bar change

--- app/render/homepage.jsx
+++ app/render/homepage.jsx
@@ -1,7 +1,7 @@
 require('../stylesheets/style.css');
 require('../stylesheets/calendar.css');
 require('../stylesheets/classMenuBar.css');
-require('../stylesheets/dashboard.css');
+require('../stylesheets/sideBar.css');
 
 import React from 'react';
 import HomePage from './pages/HomePage.jsx';

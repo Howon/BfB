5af5822a57d059fe01b21d0a2443d88447e59c0a
Ken Aizawa <k.aizawa@live.com>
2015-12-27 13:09:13 -0500

    merge

---
+++ scripts/models/threadModel.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+	ObjectId = Schema.ObjectId;
+
+var threadSchema = new Schema({
+	_id: String,
+	comments: [{
+		postedBy  : String,
+		content : String,
+		time    : Date
+	}]
+});
+
+module.exports = mongoose.model('Thread', threadSchema);
